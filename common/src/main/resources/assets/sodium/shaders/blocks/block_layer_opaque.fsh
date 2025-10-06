BZLZHH/sodium

Use `GL_MAX_TEXTURE_LOD_BIAS` as the lod-bias in texture sampling
The -4.0 bias is not enough on some hardware as
OpenGL applies the bias *before* clamping the computed
lod.

Fixes #2631

common/src/main/resources/assets/sodium/shaders/blocks/block_layer_opaque.fsh
@@ -1,13 +1,17 @@
 #version 330 core
 
+#ifndef MAX_TEXTURE_LOD_BIAS
+#error "MAX_TEXTURE_LOD_BIAS constant not specified"
+#endif
+
 #import <sodium:include/fog.glsl>
+#import <sodium:include/chunk_material.glsl>
 
 in vec4 v_Color; // The interpolated vertex color
 in vec2 v_TexCoord; // The interpolated block texture coordinates
 in float v_FragDistance; // The fragment's distance from the camera
 
-in float v_MaterialMipBias;
-in float v_MaterialAlphaCutoff;
+flat in uint v_Material;
 
 uniform sampler2D u_BlockTex; // The block texture
 
@@ -18,16 +22,16 @@ uniform float u_FogEnd; // The ending position of the shader fog
 out vec4 fragColor; // The output fragment for the color framebuffer
 
 void main() {
-    vec4 diffuseColor = texture(u_BlockTex, v_TexCoord, v_MaterialMipBias);
+    float lodBias = _material_use_mips(v_Material) ? 0.0 : float(-MAX_TEXTURE_LOD_BIAS);
 
-    // Apply per-vertex color
-    diffuseColor *= v_Color;
+    vec4 color = texture(u_BlockTex, v_TexCoord, lodBias);
+    color *= v_Color; // Apply per-vertex color modulator
 
 #ifdef USE_FRAGMENT_DISCARD
-    if (diffuseColor.a < v_MaterialAlphaCutoff) {
+    if (color.a < _material_alpha_cutoff(v_Material)) {
         discard;
     }
 #endif
 
-    fragColor = _linearFog(diffuseColor, v_FragDistance, u_FogColor, u_FogStart, u_FogEnd);
+    fragColor = _linearFog(color, v_FragDistance, u_FogColor, u_FogStart, u_FogEnd);
 }

Jokypond/sodium-pojav

common/src/main/resources/assets/sodium/shaders/blocks/block_layer_opaque.fsh
@@ -1,13 +1,17 @@
 #version 330 core
 
+#ifndef MAX_TEXTURE_LOD_BIAS
+#error "MAX_TEXTURE_LOD_BIAS constant not specified"
+#endif
+
 #import <sodium:include/fog.glsl>
+#import <sodium:include/chunk_material.glsl>
 
 in vec4 v_Color; // The interpolated vertex color
 in vec2 v_TexCoord; // The interpolated block texture coordinates
 in vec2 v_FragDistance; // The fragment's distance from the camera (cylindrical and spherical)
 
-in float v_MaterialMipBias;
-in float v_MaterialAlphaCutoff;
+flat in uint v_Material;
 
 uniform sampler2D u_BlockTex; // The block texture
 
@@ -18,16 +22,16 @@ uniform vec2 u_RenderFog; // The start and end position for border fog
 out vec4 fragColor; // The output fragment for the color framebuffer
 
 void main() {
-    vec4 diffuseColor = texture(u_BlockTex, v_TexCoord, v_MaterialMipBias);
+    float lodBias = _material_use_mips(v_Material) ? 0.0 : float(-MAX_TEXTURE_LOD_BIAS);
 
-    // Apply per-vertex color
-    diffuseColor *= v_Color;
+    vec4 color = texture(u_BlockTex, v_TexCoord, lodBias);
+    color *= v_Color; // Apply per-vertex color modulator
 
 #ifdef USE_FRAGMENT_DISCARD
-    if (diffuseColor.a < v_MaterialAlphaCutoff) {
+    if (color.a < _material_alpha_cutoff(v_Material)) {
         discard;
     }
 #endif
 
-    fragColor = _linearFog(diffuseColor, v_FragDistance, u_FogColor, u_EnvironmentFog, u_RenderFog);
+    fragColor = _linearFog(color, v_FragDistance, u_FogColor, u_EnvironmentFog, u_RenderFog);
 }